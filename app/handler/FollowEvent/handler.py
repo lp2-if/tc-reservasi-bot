5b7ed2ab4eefa7c1d612943178ad1bb46a671a8e
Rogo Jagad Alit <rogojagadalit@gmail.com>
2018-11-20 22:33:15 +0700

    Update handler.py

--- app/handler/FollowEvent/handler.py
+++ app/handler/FollowEvent/handler.py
@@ -16,11 +16,12 @@ class FollowEventHandler:
         message1 = "Halo " + firstName + \
             ", terima kasih sudah menambahkan saya sebagai teman.\nSaya dapat membantu anda terkait informasi reservasi ruangan di Departemen Informatika ITS.\n\n"
 
-        message2 = "Beberapa perintah yang dapat anda berikan: \n\n"
+        message2 = "Beberapa perintah yang dapat kamu berikan: \n\n"
 
-        command_msg = "1. !today untuk melihat ruangan yang tersedia\n"
-        command_msg += "2. !today [SPASI] nama ruangan untuk melihat jadwal kegiatan di ruangan tersebut untuk hari ini\n"
-        command_msg += "3. !help untuk mengetahui perintah yang tersedia\n"
+        command_msg = "1. !today\nUntuk melihat ruangan yang tersedia\n"
+        command_msg += "2. !today <nama_ruang>\nUntuk melihat jadwal kegiatan di ruangan tersebut untuk hari ini\n"
+        command_msg += "3. !status <nama_kamu>\nUntuk mengecek status reservasi kamu\n\n"
+        command_msg += "4. !help\nUntuk mengetahui perintah yang tersedia\n"
 
         message_body = message1 + message2 + command_msg
 
