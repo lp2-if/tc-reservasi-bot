f21e9eba9c55c1d2dcaa52ea12bf27646b2b56fa
rogojagad <rogojagadalit@gmail.com>
2018-11-22 14:25:28 +0700

    remove old feature_today implementation

--- app/handler/MessageEvent/handler.py
+++ app/handler/MessageEvent/handler.py
@@ -11,9 +11,11 @@ from linebot.models import (
 from app.line import line_bot_api, line_handler
 from app.utils import MessageFactory
 
+from app.handler.MessageEvent.Features.TodayFeature import TodayFeature
+
 class MessageEventHandler:
     def __init__(self):
-        pass
+        self.feature_today = TodayFeature()
 
     def handle(self, event):
         try:
@@ -30,7 +32,7 @@ class MessageEventHandler:
         text = event.message.text.strip().lower()
 
         if (text.startswith("!today")):
-            self.feature_today(event)
+            self.feature_today.run(event)
         elif (text.startswith("!help")):
             self.feature_help(event)
         elif (text.startswith("!status")):
@@ -120,109 +122,3 @@ class MessageEventHandler:
                     TextSendMessage(text=message.strip())
                 ]
             )
-
-    def feature_today(self, event):
-        text = event.message.text.strip()
-        commands = text.split(' ')
-        today = datetime.datetime.today()
-        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-
-        if (len(commands) == 1):
-            self.send_room_list(event)
-            return
-
-        roomname = commands[1]
-
-        if not(self.is_room_exists(roomname)):
-            self.reply(event, MessageFactory.room_not_found_message(roomname))
-            return
-
-        payload = {
-            "start": today.strftime("%Y-%m-%d"),
-            "end": tomorrow.strftime("%Y-%m-%d")
-        }
-
-        url = 'http://reservasi.if.its.ac.id/calendar/accepted/%s' % roomname
-
-        schedules = requests.get(url, payload).json()
-
-        title_message = 'Kegiatan di %s untuk hari ini:' % roomname
-
-        message = ''
-
-        for schedule in schedules:
-            messagePart = '%(title)s\n%(start)s - %(end)s\n\n' % {
-                'title': schedule['title'],
-                'start': schedule['start'].split(' ')[1],
-                'end':  schedule['end'].split(' ')[1]
-            }
-            message += messagePart
-            
-        if (message == ''):
-            line_bot_api.reply_message(
-                event.reply_token,
-                [
-                    TextSendMessage(
-                        text="Hari ini tidak ada kegiatan di %s" % roomname)
-                ]
-            )
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                [
-                    TextSendMessage(text=title_message),
-                    TextSendMessage(text=message.strip())
-                ]
-            )
-
-    def is_room_exists(self, roomname):
-        exist_rooms = [
-            'IF-101',
-            'IF-102',
-            'IF-103',
-            'IF-104',
-            'IF-105A',
-            'IF-105B',
-            'IF-106',
-            'IF-108',
-            'IF-111',
-            'IF-112',
-            'RAPAT1',
-            'RAPAT2',
-            'SIDANG',
-            'AULA',
-            'RTV',
-            'LP1',
-            'AJK',
-            'LP2'
-        ]
-
-        return roomname in exist_rooms
-
-    def send_room_list(self, event):
-        url = 'http://reservasi.if.its.ac.id/calendar'
-        response = requests.get(url).content
-        dom = pq(response)
-        carousel_columns = []
-        options = dom("#room_select option:not([selected])")
-        actions = []
-        for option in options:
-            room_name = option.text
-            actions.append(
-                MessageAction(label='%s hari ini' %
-                              room_name, text='!today %s' % room_name)
-            )
-            if (len(actions) == 3):
-                carousel_columns.append(
-                    CarouselColumn(text="Daftar ruangan %s" % str(
-                        len(carousel_columns) + 1), actions=actions)
-                )
-                actions = []
-        carousel_template = CarouselTemplate(
-            columns=carousel_columns
-        )
-        template_message = TemplateSendMessage(
-            alt_text='Daftar ruangan', template=carousel_template
-        )
-        line_bot_api.reply_message(event.reply_token, template_message)
-
